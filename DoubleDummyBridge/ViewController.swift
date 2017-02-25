d9ca75d4aa20ef862f0142ade062970fa8730801
Fabian von Löbbecke <Fabi@fabians-mbp.fritz.box>
2017-02-25 12:57:48 +0100

    Initial Commit

---
+++ DoubleDummyBridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoubleDummyBridge
+//
+//  Created by Fabian von Löbbecke on 25.02.17.
+//  Copyright © 2017 Fabian von Löbbecke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
